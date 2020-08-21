c58ee8a2c23088afe662a11fb678bb14d62f7cd4
annopkomol <komolviior@gmail.com>
2020-08-21 12:08:00 +0700

    setup eslint

---
+++ web/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended",
+    "prettier",
+    "prettier/vue"
+  ],
+  parserOptions: {
+    parser: "babel-eslint"
+  },
+  rules: {
+    "global-require": 0,
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+  }
+};
