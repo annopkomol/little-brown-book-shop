da8f3cf543f2d95a437eae5eb0acb855b9acb29c
annopkomol <komolviior@gmail.com>
2020-08-21 12:12:54 +0700

    add views

---
+++ web/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <pos-template></pos-template>
+</template>
+
+<script>
+
+import PosTemplate from "@/components/templates/PosTemplate"
+
+export default {
+  name: "Home",
+  components: { PosTemplate },
+}
+</script>
---
+++ web/src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <login-form></login-form>
+  </div>
+</template>
+
+<script>
+import LoginForm from "@/components/organisms/LoginForm.vue"
+
+export default {
+  name: "Login",
+  components: { LoginForm },
+}
+</script>
+
