54144aa7546bdfc7e8ae427afcc67b2e28235cb8
annopkomol <komolviior@gmail.com>
2020-08-21 11:49:55 +0700

    fix typo

--- api/domain/book.go
+++ api/domain/book.go
@@ -3,10 +3,10 @@ package domain
 import "github.com/shopspring/decimal"
 
 type Book struct {
-	ID     int             `json:"id" db:"id"`
-	Tittle string          `json:"tittle" db:"tittle"`
-	Cover  string          `json:"cover" db:"cover"`
-	Price  decimal.Decimal `json:"price" db:"price"`
+	ID    int             `json:"id" db:"id"`
+	Title string          `json:"title" db:"title"`
+	Cover string          `json:"cover" db:"cover"`
+	Price decimal.Decimal `json:"price" db:"price"`
 }
 
 type BookService interface {
