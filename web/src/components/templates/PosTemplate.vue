cdf5bcb19ec867d50f4ec04c3356b560579c4c75
annopkomol <komolviior@gmail.com>
2020-08-21 12:12:36 +0700

    add point of sale template

---
+++ web/src/components/templates/PosTemplate.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-row>
+    <v-col md="8">
+      <item-list></item-list>
+    </v-col>
+    <v-col md="4">
+      <order-list></order-list>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import ItemList from "@/components/organisms/ItemList.vue"
+import OrderList from "@/components/organisms/OrderList"
+
+export default {
+  name: "PosTemplate",
+  components: { ItemList, OrderList },
+}
+</script>
+
+<style scoped>
+
+</style>
